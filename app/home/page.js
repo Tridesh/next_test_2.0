d6fc1ce5aa6dd0fe4a9f3898b63d430e5746646a
Tridesh <tridesh2005@gmail.com>
2024-05-11 12:08:36 +0530

    major update

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
-
+import "./style.css";
 export default function Home() {
     return (
-        <div className="navbar">
+        <div>
+            <div className="navbar">
             <a href = "/">Learn</a>
             <a href="/signin">Sign In</a>
             <a href="/signup">Sign Up</a>
+            </div>
         </div>
+        
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,95 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import logo from "/public/pandalogo.jpg";
+import "./globals.css";
+import pic from "/public/panda.jpg";
+// import { useRouter } from "next/Home";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="navbar">
+        <div className="nav_right">
+          <a href="./Home">Learn Here</a>
+          <a href="./signin">Sign In</a>
+          <a href = "./signup">Sign Up</a>
+        </div>
+        <div className="logo">
+          <Image className = "pandalogo" src={logo} alt="logo" width={60}></Image>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="text">
+        <div>
+        "Unravel the enigmatic world of pandas:
+        </div>
+        <div>
+        Discover their playful antics,
+        </div>
+        <div>
+        curious habits, and mysterious ways here."
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <Image className="pic" src={pic} alt="panda photo" width={600}></Image>
       </div>
-    </main>
+      <div className="footer">
+        <button><div>Footer 1</div></button>
+        <button><div>Footer 2</div></button>
+        <button><div>Footer 3</div></button>
+      </div>
+    </div>
   );
 }
